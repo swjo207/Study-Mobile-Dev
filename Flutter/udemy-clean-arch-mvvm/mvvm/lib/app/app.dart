a911f7506e3a30dc293e070e02a1d75601fe02d9
swjo207 <swjo207@gmail.com>
2022-01-08 02:03:51 +0900

    styles and theme

---
+++ Flutter/udemy-clean-arch-mvvm/mvvm/lib/app/app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm/presentation/theme_manager.dart';
+
+class MyApp extends StatefulWidget {
+  // const MyApp({Key? key}) : super(key: key); // 기본 생성자
+  MyApp._internal(); // private named constructor
+  int appState = 0;
+
+  static final MyApp instance = MyApp._internal(); // 싱글 인스턴스 (Singleton)
+
+  factory MyApp() => instance; // factory for the class instance
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: getApplicationTheme(),
+    );
+  }
+}
