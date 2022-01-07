00f3826b8bed41e01f0c349b2ac177bdfad6bdb3
swjo207 <swjo207@gmail.com>
2022-01-08 02:05:39 +0900

    resources

--- Flutter/udemy-clean-arch-mvvm/mvvm/lib/presentation/color_manager.dart
+++ Flutter/udemy-clean-arch-mvvm/mvvm/lib/presentation/color_manager.dart
@@ -6,6 +6,12 @@ class ColorManager {
   static Color grey = HexColor.fromHex("#737477");
   static Color lightGrey = HexColor.fromHex("#9e9e9e");
   static Color primaryOpacity70 = HexColor.fromHex("#b3ed9728");
+  // new colors
+  static Color darkPrimary = HexColor.fromHex("#d17d11");
+  static Color grey1 = HexColor.fromHex("#707070");
+  static Color grey2 = HexColor.fromHex("#797979");
+  static Color white = HexColor.fromHex("#FFFFFF");
+  static Color error = HexColor.fromHex("#E61F34");
 }
 
 extension HexColor on Color {
