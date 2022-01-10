8642f26543db9c1fae5c63ad6871653dea2d05ef
swjo207 <swjo207@gmail.com>
2022-01-10 21:59:24 +0900

    lec 25 onboarding screen

--- Flutter/udemy-clean-arch-mvvm/mvvm/lib/presentation/splash/splash.dart
+++ Flutter/udemy-clean-arch-mvvm/mvvm/lib/presentation/splash/splash.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:mvvm/presentation/resources/assets_manager.dart';
 import 'package:mvvm/presentation/resources/color_manager.dart';
+
+import '../resources/routes_manager.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({Key? key}) : super(key: key);
@@ -10,6 +14,24 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  Timer? _timer;
+  _startDelay() {
+    _timer = Timer(Duration(seconds: 2), _goNext);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _startDelay();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,5 +42,9 @@ class _SplashViewState extends State<SplashView> {
         ),
       ),
     );
+  }
+
+  void _goNext() {
+    Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
   }
 }
